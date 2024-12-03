ae804138f45c5f0913b57833c09cb78c4007d31f
Renan Silva <renanwds@gmail.com>
2024-12-02 23:32:19 -0300

    Update project

--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
-function calcularTabuada() {
-    const number = document.getElementById("number").value;
-    const result = document.getElementById("result");
-    let tabuada = '';
+const numberElement = document.getElementById("number");
+const resultElement = document.getElementById("result");
 
-    for (let i=1; i<=10; i++) {
-        tabuada += `${number} x ${i} = ${number * i}` + "<br>";
-    }
-    
-    result.innerHTML = tabuada;
+function calcularTabuada() {
+  const number = numberElement.value;
+  resultElement.innerHTML = "";
+
+  for (let i = 1; i <= 10; i++) {
+    const result = `${number} \u00D7 ${i} = ${number * i}`;
+
+    const element = document.createElement("p");
+
+    element.innerText = result;
+
+    resultElement.appendChild(element);
+  }
 }
